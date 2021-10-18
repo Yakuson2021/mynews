3745d2189f102d16a30983215248788ec93f4134
名前 <>
2021-10-18 11:27:53 +0000

    second commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -7,5 +7,23 @@ use App\Http\Controllers\Controller;
 
 class ProfileController extends Controller
 {
-    //
+  public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }  //
 }
