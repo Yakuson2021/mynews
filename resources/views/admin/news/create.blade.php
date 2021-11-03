dd1d4a726c07732cf975dba091d3c07758a76a1b
名前 <>
2021-11-03 16:21:02 +0000

    Laravel14 カリキュラムでエラー発生中

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,12 +1,14 @@
 @extends('layouts.admin')
-@section('title', '自己紹介')
+@section('title', 'ニュースの新規作成')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>自己紹介</h2>
-                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+                <h2>ニュース新規作成</h2>
+                <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
+
+ // 以下を追記
 
                     @if (count($errors) > 0)
                         <ul>
@@ -15,33 +17,22 @@
                             @endforeach
                         </ul>
                     @endif
-
-<div class="form-group row">
-                        <label class="col-md-2">氏名(name)</label>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
                             <input type="text" class="form-control" name="title" value="{{ old('title') }}">
                         </div>
                     </div>
-
-<div class="form-group row">
-                        <label class="col-md-2">性別(gender)</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="title" value="{{ old('title') }}">
-                        </div>
-                    </div>
-
-<div class="form-group row">
-                        <label class="col-md-2">趣味(hobby)</label>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
                             <textarea class="form-control" name="body" rows="20">{{ old('body') }}</textarea>
                         </div>
                     </div>
-
-<div class="form-group row">
-                        <label class="col-md-2">自己紹介欄(introduction)</label>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="title">画像</label>
                         <div class="col-md-10">
-
-<input type="file" class="form-control-file" name="image">
+                            <input type="file" class="form-control-file" name="image">
                         </div>
                     </div>
                     {{ csrf_field() }}
