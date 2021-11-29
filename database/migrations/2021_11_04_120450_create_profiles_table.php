b65f162963dca29b0228d64814e3c570bed1c27d
名前 <>
2021-11-29 12:39:04 +0000

    1129

--- database/migrations/2021_11_04_120450_create_profiles_table.php
+++ database/migrations/2021_11_04_120450_create_profiles_table.php
@@ -14,7 +14,7 @@ class CreateProfilesTable extends Migration
     public function up()
     {
         Schema::create('profiles', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->integer('profile_id');
             $table->string('name');
             $table->string('gender');
             $table->string('hobby'); 
--- database/migrations/2021_11_23_122434_create_histories_table.php
+++ database/migrations/2021_11_23_122434_create_histories_table.php
@@ -14,7 +14,8 @@ class CreateHistoriesTable extends Migration
     public function up()
     {
         Schema::create('histories', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->integer('profile_id');
+            $table->string('name');
             $table->integer('news_id');
             $table->string('edited_at');
             $table->timestamps();
